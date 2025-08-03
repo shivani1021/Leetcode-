4feaceffeb98a7de78166ac8eeb0e391548e9c03
Shivani Mahato <shivanimahato182@gmail.com>
2025-08-04 02:00:06 +0530

    Time: 2 ms (55.47%), Space: 59 MB (46.61%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+let canJump = function(nums) {
+    let maxreach=0;
+    for(let i=0; i <nums.length; i++){
+        if(i>maxreach){
+            return false;
+        }
+        maxreach = Math.max(maxreach, i+nums[i]);
+    }
+    return  true;
+
+};
