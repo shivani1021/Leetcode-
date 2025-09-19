13a48d7a4b8f3762106f787e481ef4758efdf866
Shivani Mahato <shivanimahato182@gmail.com>
2025-09-19 11:01:02 +0530

    Time: 0 ms (100%), Space: 56.3 MB (92.75%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let count = 0;
+
+    for(let i =0; i<nums.length ;i++){
+        if(nums[count] != nums[i]){
+            count++;
+        nums[count] = nums[i]
+        }
+    }
+
+    return  count+1;
+    
+};
